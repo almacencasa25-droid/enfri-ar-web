3cfd111d3ababdc15f59757bc385fabe61acdc2c
almacencasa25-droid <almacencasa25@gmail.com>
2026-09-18 14:27:27 -0300

    app/admin/presupuestos/listado/[id]/orden-trabajo/pdf/actions.ts

---
+++ app/admin/presupuestos/lib/generarConformidadPdf.ts
@@ -0,0 +1,31 @@
+CONFORMIDAD
+CONF-001051-02
+Según Presupuesto N° 1051
+
+CLIENTE / ESTABLECIMIENTO
+[dato del presupuesto]
+
+TÉCNICO RESPONSABLE
+[dato tomado de la OT / snapshot]
+
+TRABAJO REALIZADO
+[detalle histórico]
+
+OBSERVACIONES
+[si existen]
+
+FECHA DE CONFORMIDAD: ______________________
+
+FECHA DE FINALIZACIÓN: _____________________
+
+RESPONSABLE DEL ESTABLECIMIENTO
+
+Nombre y apellido: _________________________
+
+DNI: ______________________________________
+
+Cargo / función: ___________________________
+
+Firma: ____________________________________
+
+Aclaración: ________________________________
